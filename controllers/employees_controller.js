41824d54b8273c68be61362d3662487c7577e25f
PiyushNimgire <106684605+PiyushNimgire@users.noreply.github.com>
2023-06-14 02:24:37 +0530

    view is created for register page

--- controllers/employees_controller.js
+++ controllers/employees_controller.js
@@ -1,6 +1,5 @@
 module.exports.register = (req, res) => {
-    res.send("<h1>Registration</h1>")
-    // res.render('employee_register', {
-    //     title: "Registartion Page"
-    // })
+    res.render('employee_register', {
+        title: "Registartion Page"
+    })
 }
